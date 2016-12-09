859ac4ece8276a9e134891666ab136e345aa112b
g00p <michaels138@gmail.com>
2016-12-09 15:27:47 -0500

    functions for doublecheck

--- functions.py
+++ functions.py
@@ -1,38 +1,62 @@
 import os, shutil, pprint
-from openpyxl import load_workbook
-
-fileNames = {}
-alts = {}
-scriptNames = {}
-missingFiles = {}
-lineTotal = 0
+from openpyxl import load_workbook, worksheet
+from openpyxl.utils import get_column_letter
 
 
-def getFileNames():
+# fileNames = {}
+# alts = {}
+# scriptNames = {}
+# missingFiles = {}
+# lineTotal = 0
+
+# TODO - implement ability to deal with multi-length filenames
+
+def split_file_names(directory):
     '''
-    Search within cwd, adding all .wav files to fileNames
-    :return:
+    split .wav file names into lists and add them to a list
+    :return list of lists:
     '''
-    for i in os.listdir('.'):
+    file_name_list = []
+    for i in os.listdir(directory):
         if not str(i).endswith('.wav'):
             continue
-        if not in fileNames:
-            fileNames[i] = True
+        # Split filename into a list.
+        file_name_list.append(i.split('_'))
+    return file_name_list
+    # filePath = os.path.abspath(i)
+    # # Use first 2 indexes in fileNameList to create correct path for file.
+    # destPath = ('.\\' + str(masterFolder) + '\\' + str(fileNameList[0]) + '\\' + str(fileNameList[1]))
+    # # Copy file to correct path in file structure.
+    # if not os.path.exists(destPath):
+    #     os.makedirs(destPath)
+    # shutil.copy(filePath, destPath)
+
+
+def tally_alts(split_list):
+    '''
+    iterate over list of lists, placing the first 4 indeces into a dictionary as a key with value 1
+    and incrementing that value per identical entry
+    :return:
+    '''
+    filename_dict = {}
+    for i in split_list:
+        if '{}_{}_{}_{}'.format(i[0], i[1], i[2], i[3]) not in filename_dict:
+            filename_dict['{}_{}_{}_{}'.format(i[0], i[1], i[2], i[3])] = 1
         else:
-            alts[i] += 1
+            filename_dict['{}_{}_{}_{}'.format(i[0], i[1], i[2], i[3])] += 1
 
 
-def checkMissing(script, file_names):
+def checkMissing(script_file_list, file_names):
     '''
     compare dicts script and files, returns a with missing values
-    :param script:
+    :param script_file_list:
     dictionary
     :param file_names:
     dictionary
     :return:
     '''
     missing_files = {}
-    for i in script:
+    for i in script_file_list:
         if i not in file_names:
             missing_files[i] = True
     return missing_files
@@ -40,8 +64,9 @@ def checkMissing(script, file_names):
 
 def getWorkbook(directory):
     '''
-    iterate a directory to find .xlsx files and adds them to a list.
+    iterate over a directory to find .xlsx files and adds them to a list.
     :return:list
+    :param directory:
     '''
     workbook_names = []
     for i in os.listdir(directory):
@@ -62,7 +87,7 @@ def choose_workbook(directory):
     to a list which is returned only if there is more than one entry.
     otherwise exit()
     :param directory:
-    :return: list or exit()
+    :return: string or exit()
     '''
 
     workbook_names = getWorkbook(directory)
@@ -78,10 +103,16 @@ def choose_workbook(directory):
         exit()
 
 
-def get_workbook(workbook_path):
-    wb = load_workbook(filename=str(workbook_path))
-    return wb
+# def get_workbook(workbook_path):
+#     wb = load_workbook(filename=str(workbook_path))
+#     return wb
 
+def open_workbook(path):
+    '''
+    opens target path as a workbook object
+    :param path:
+    :return: wb object
+    '''
 
 def choose_wb_sheet(wb):
     '''
@@ -104,17 +135,59 @@ def choose_wb_sheet(wb):
     return str(ws_list[sheet_number])
 
 
-def find_ws_column(ws, keyword):
+def find_ws_column(ws, keyword, range):
     '''
-    iterate rows in ws, searching for value 'KEYWORD'
+    iterate rows in ws, searching for string 'KEYWORD'
     :param ws: ws object
     :param keyword: string to search for in column
-    :return: string of chosen sheet from s
+    :param range: range string of worksheet
+    :return: index of column that begins with "keyword"
     '''
-    column_list = []
-    for col in ws.columns[0]:
-        column_list.append(col.value)
-    if str(keyword) in column_list:
+    for row in ws.iter_rows(range):
+        for cell in row:
+            if cell.value == keyword:
+                return cell.column
 
 
-def get_scriptNames(wb):
+def get_script_names(ws, column):
+    '''
+    loops over a column, discarding empty cells and creates a list containing the values
+    :param ws: worksheet containing column
+    :param column: column as int to be looped
+    #:param list_name: list to add values to
+    :return:
+    '''
+    list_name = []
+    column_letter = get_column_letter(column)
+    for row in ws.iter_rows('{}{}:{}{}'.format(str(column_letter),
+                                               str(ws.min_row),
+                                               str(column_letter),
+                                               str(ws.max_row))):
+        for cell in row:
+            if cell.value:
+                list_name.append(cell.value)
+    return list_name
+
+
+def main():
+    dir_to_use = '.'
+    file_name_list = split_file_names(dir_to_use)
+    print('Got {} files!'.format(len(file_name_list)))
+    wb = load_workbook(str(dir_to_use) + "//" +
+                       str(choose_workbook(dir_to_use)),
+                       data_only=True, read_only=True)
+
+    ws = wb.get_sheet_by_name(choose_wb_sheet(wb))
+
+    keyword = str(input('Enter the name of the column '
+                        'header you are looking for (e.g. FILENAME):'))
+
+    script_file_list = get_script_names(ws, (find_ws_column(ws, keyword, 'A1:Q3')))
+
+    missing_files = checkMissing(script_file_list, file_name_list)
+    print('The following files are missing:')
+    print(pprint.pprint(missing_files.keys))
+
+
+if __name__ == '__main__':
+    main()
