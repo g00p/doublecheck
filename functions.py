5aaf4f154eea0238263d45f44308e1aa08a10b3c
g00p <michaels138@gmail.com>
2017-02-28 15:41:31 -0500

    fixed lots of bugs, got basic functionalities up and running!!

--- functions.py
+++ functions.py
@@ -1,15 +1,24 @@
 import os, shutil, pprint
 from openpyxl import load_workbook, worksheet
 from openpyxl.utils import get_column_letter
-
-
-# fileNames = {}
-# alts = {}
-# scriptNames = {}
-# missingFiles = {}
-# lineTotal = 0
+import sys
+import time
 
 # TODO - implement ability to deal with multi-length filenames
+
+def choose_working_dir():
+    print('Work in the current directory?\n\n{}'
+          .format(os.path.dirname(os.path.realpath(__file__))))
+    if yes_or_no():
+        return str(os.path.dirname(os.path.realpath(__file__)))
+    else:
+        path = str(input('Please enter the full path to the directory'
+                         ' you want to work in \n\n'))
+        if not path.endswith('\\'):
+            return "{}\\".format(path)
+        else:
+            return path
+
 
 def split_file_names(directory):
     '''
@@ -23,13 +32,6 @@ def split_file_names(directory):
         # Split filename into a list.
         file_name_list_split.append(i.split('_'))
     return file_name_list_split
-    # filePath = os.path.abspath(i)
-    # # Use first 2 indexes in fileNameList to create correct path for file.
-    # destPath = ('.\\' + str(masterFolder) + '\\' + str(fileNameList[0]) + '\\' + str(fileNameList[1]))
-    # # Copy file to correct path in file structure.
-    # if not os.path.exists(destPath):
-    #     os.makedirs(destPath)
-    # shutil.copy(filePath, destPath)
 
 
 def get_file_names_raw(directory):
@@ -45,6 +47,8 @@ def get_file_names_raw(directory):
         file_name_list_raw.append(i)
     return file_name_list_raw
 
+
+# TODO - Make this work!
 def tally_alts(split_list):
     '''
     iterate over list of lists, placing the first 4 indeces into a dictionary as a key with value 1
@@ -105,7 +109,7 @@ def choose_workbook(directory):
 
     workbook_names = []
     workbook_names.append(getWorkbook(directory))
-    if not workbook_names:
+    if False in workbook_names:
         print('No .xlsx files found. Exiting...')
         exit()
     if len(workbook_names) == 1:
@@ -117,9 +121,18 @@ def choose_workbook(directory):
         exit()
 
 
-# def get_workbook(workbook_path):
-#     wb = load_workbook(filename=str(workbook_path))
-#     return wb
+def yes_or_no():
+    yes = set(['yes', 'y', 'ye', ''])
+    no = set(['no', 'n'])
+
+    choice = input().lower()
+    if choice in yes:
+        return True
+    elif choice in no:
+        return False
+    else:
+        sys.stdout.write("Please respond with 'yes' or 'no'\n")
+
 
 def open_workbook(path):
     '''
@@ -127,6 +140,7 @@ def open_workbook(path):
     :param path:
     :return: wb object
     '''
+
 
 def choose_wb_sheet(wb):
     '''
@@ -141,7 +155,7 @@ def choose_wb_sheet(wb):
         print('{}. {}'.format(ws_list.index(i), str(i)))
     while True:
         try:
-            sheet_number = int(input("Which # sheet do you want to open?"))
+            sheet_number = int(input("Which # sheet do you want to open?\n"))
         except ValueError:
             pass
         if (sheet_number > -1) and (sheet_number <= len(ws_list) - 1):
@@ -183,8 +197,37 @@ def get_script_names(ws, column):
     return list_name
 
 
+def get_time_stamp():
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    return str("\n\n{}\n".format(timestr))
+
+
+def gameloft_builder(master_folder, working_dir):
+    """
+    iterate over wav files in a directory, and place them in gameloft build file structure
+    e.g. 'M1151_INTRO_01_VAHN.wav'
+    :param master_folder: string - Name of parent folder created for file structure
+    :param working_dir: directory path - directory to work in
+    :return: nothing
+    """
+    for i in os.listdir(working_dir):
+        if not str(i).endswith('.wav'):
+            continue
+        # Split filename into a list.
+        file_name_list = i.split('_')
+        # file_path = os.path.abspath(i)
+        file_path = str(working_dir + i)
+        # Use first 2 indexes in fileNameList to create correct path for file.
+        dest_path = ('.\\' + str(master_folder) + '\\' + str(file_name_list[0]) + '\\' + str(file_name_list[1]))
+        # Copy file to correct path in file structure.
+        if not os.path.exists(dest_path):
+            os.makedirs(dest_path)
+        shutil.copy(file_path, dest_path)
+    print('you did it champ!!')
+
+
 def main():
-    dir_to_use = 'c://doublechecktest//'
+    dir_to_use = choose_working_dir()
     file_name_list = get_file_names_raw(dir_to_use)
     print('Got {} files!'.format(len(file_name_list)))
     wb = load_workbook(str(dir_to_use) + "//" +
@@ -194,14 +237,31 @@ def main():
     ws = wb.get_sheet_by_name(choose_wb_sheet(wb))
 
     keyword = str(input('Enter the name of the column '
-                        'header you are looking for (e.g. FILENAME):'))
+                        'header you are looking for (e.g. FILENAME):\n'))
 
     script_file_list = get_script_names(ws, (find_ws_column(ws, keyword, 'A1:Q3')))
 
     missing_files = checkMissing(script_file_list, file_name_list)
     print('The following files are missing:')
-    for key in missing_files:
-        print(key)
+    if missing_files:
+        for key in missing_files:
+            if key == 'FILENAME':
+                continue
+            print(key)
+        with open('Missing-files.txt', 'a') as text_file:
+            text_file.write(get_time_stamp())
+        for key in missing_files:
+            if key == 'FILENAME':
+                continue
+            with open('Missing-files.txt', 'a') as text_file:
+                text_file.write('{}\n'.format(key))
+    print('Missing filenames written to Missing-files.txt')
+
+    sys.stdout.write('Begin sorting files?\n')
+    if yes_or_no():
+        gameloft_builder(str(input('Enter master folder name \n e.g. "DH5_GU15"\n')), dir_to_use)
+    else:
+        print('Bye Bye!')
 
 if __name__ == '__main__':
     main()
